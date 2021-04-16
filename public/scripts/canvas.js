035810f0442e8765ba490628c8b33de9fd045bbd
Noah R-G <27902614+noah-r-g@users.noreply.github.com>
2021-04-16 05:29:29 -0400

    major frontend work including canvas and regionBar

--- public/scripts/canvas.js
+++ public/scripts/canvas.js
@@ -1,40 +1,158 @@
 let canvas = document.getElementById("canvas");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
 let ctx = canvas.getContext("2d");
 
-let scaleFactor = .85;
-let xOffset = 350;
-let yOffset = 20;
+let scaleFactor = 1;
+let xOffset = 0;
+let yOffset = 0;
 
-function main()
+let mouse =
 {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+	x: undefined,
+	y: undefined
+};
+
+canvas.addEventListener('mousedown', e =>
+{
+    mouse.x = e.offsetX;
+    mouse.y = e.offsetY;
+});
+
+function Region(regionObject)
+{
+    this.name = regionObject.name;
+    this.type = regionObject.type;
+    this.adjacentRegionNames = regionObject.adjacentRegionNames;
+
+    this.coordinates = regionObject.coordinates;
+    this.numberOfCoordinates = this.coordinates.length;
+    this.industrialization = regionObject.industrialization;
+
+    this.arrayOfXCoordinates = [];
+    this.arrayOfYCoordinates = [];
+
+    this.draw = function()
+    {
+        if (gameApp.game.regions.filter(region => region.name == this.name)[0].player != null)
+        {
+            ctx.fillStyle = playerColors[gameApp.game.regions.filter(region => region.name == this.name)[0].player];
+        }
+        else if (this.type == 'land')
+        {
+            ctx.fillStyle = '#0c6d13';
+        }
+        else
+        {
+            ctx.fillStyle = '#0c3468';
+        }
+        this.arrayOfXCoordinates = [];
+        this.arrayOfYCoordinates = [];
+        ctx.beginPath();
+        ctx.moveTo(this.coordinates[0].x * scaleFactor + xOffset, this.coordinates[0].y * scaleFactor + yOffset);
+        for (coordinate of this.coordinates)
+        {
+            ctx.lineTo(coordinate.x * scaleFactor + xOffset, coordinate.y * scaleFactor + yOffset);
+            this.arrayOfXCoordinates.push(coordinate.x * scaleFactor + xOffset);
+            this.arrayOfYCoordinates.push(coordinate.y * scaleFactor + yOffset);
+        }
+
+        ctx.lineTo(this.coordinates[0].x * scaleFactor + xOffset, this.coordinates[0].y * scaleFactor + yOffset);
+        ctx.strokeStyle = "#06111B";
+        ctx.lineWidth = 3;
+        if (gameApp.currentlySelectedRegion != null && gameApp.currentlySelectedRegion.name == this.name)
+        {
+            //ctx.strokeStyle = "#FFF";
+            //ctx.lineWidth = 5;
+            //if (regionArray.indexOf(this) != regionArray.length - 1)
+            //{
+            //    makeRegionTheLastDrawn(regionArray, regionArray.indexOf(this), regionArray.length - 1);
+            //}
+        }
+        ctx.stroke();
+        ctx.closePath();
+        ctx.fill();
+    }
+    this.update = function()
+    {
+        var i, j, inRegion = false;
+        for( i = 0, j = this.numberOfCoordinates-1; i < this.numberOfCoordinates; j = i++ )
+        {
+            if( ( ( this.arrayOfYCoordinates[i] > mouse.y ) != ( this.arrayOfYCoordinates[j] > mouse.y ) ) && ( mouse.x < ( this.arrayOfXCoordinates[j] - this.arrayOfXCoordinates[i] ) * ( mouse.y - this.arrayOfYCoordinates[i] ) / ( this.arrayOfYCoordinates[j] - this.arrayOfYCoordinates[i] ) + this.arrayOfXCoordinates[i] ) )
+            {
+                inRegion = !inRegion;
+            }
+        }
+        if(inRegion)
+        {
+            gameApp.currentlySelectedRegion = gameApp.game.regions.filter(region => region.name == this.name)[0];
+            gameApp.updateRegionBar();
+        }
+        this.draw();
+    }
+}
+
+let regionArray = [];
+let colorArray = ["#88CEC7","#CE888F","#B2CE88","#A488CE"];
+let playerArray = [];
+let playerColors = {};
+
+function initialize()
+{
     if (gameApp.game != null)
     {
         for (region of gameApp.game.map.regions)
         {
-            if (region.type == 'land')
-            {
-                ctx.fillStyle = '#0c6d13';
-            }
-            else
-            {
-                ctx.fillStyle = '#0c3468';
-            }
-            ctx.beginPath();
-            ctx.moveTo(region.coordinates[0].x * scaleFactor + xOffset, region.coordinates[0].y * scaleFactor + yOffset);
-            for (coordinate of region.coordinates)
-            {
-                ctx.lineTo(coordinate.x * scaleFactor + xOffset, coordinate.y * scaleFactor + yOffset);
-            }
-            ctx.lineTo(region.coordinates[0].x * scaleFactor + xOffset, region.coordinates[0].y * scaleFactor + yOffset);
-            ctx.stroke();
-            ctx.closePath();
-            ctx.fill();
+            regionArray.push(new Region(region));
         }
+        playerArray = gameApp.game.players;
+        let count = 0;
+        for (player of playerArray)
+        {
+            playerColors[player._id] = colorArray[count];
+            count++;
+        }
+
+        main();
+    }
+    else
+    {
+        requestAnimationFrame(initialize);
+    }
+}
+
+function main()
+{
+    //if (gameApp.leftoverX != null)
+    //{
+        //scaleFactor = gameApp.leftoverX / 1600;
+        //canvas.width = gameApp.leftoverX - 300;
+        //xOffset = (gameApp.leftoverX - canvas.width);
+    //}
+    //else
+    //{
+    //    canvas.width = 875;
+    //}
+    canvas.width = 800;
+    canvas.height = canvas.width;
+    scaleFactor = canvas.width * .001;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (region of regionArray)
+    {
+        region.update();
     }
     requestAnimationFrame(main);
 }
 
-main();
+function makeRegionTheLastDrawn(arr, old_index, new_index)
+{
+    if (new_index >= arr.length)
+    {
+        var k = new_index - arr.length + 1;
+        while (k--)
+        {
+            arr.push(undefined);
+        }
+    }
+    arr.splice(new_index, 0, arr.splice(old_index, 1)[0]);
+}
+
+initialize();
--- public/scripts/gameApp.js
+++ public/scripts/gameApp.js
@@ -13,13 +13,13 @@ async function updateGameApp() {
     gameApp.user = playerSession.user;
     gameApp.game = playerSession.game;
     gameApp.player = playerSession.player;
+    gameApp.windowResize();
 
     console.log(JSON.stringify(playerSession.player));
     console.log(JSON.stringify(playerSession.game.state));
 }
 
 socket.on("updateGameApp", updateGameApp);
-
 
 // captures all data from player-session
 let gameApp = new Vue({
@@ -33,10 +33,32 @@ let gameApp = new Vue({
         //ctx: null,
         agricultureMarketRequest: null,
         miningMarketRequest: null,
-        syntheticsMarketRequest: null
-
+        syntheticsMarketRequest: null,
+        leftoverX: null,
+        currentlySelectedRegion: null
     },
-    methods: {
+    created()
+    {
+        window.addEventListener("resize", this.windowResize);
+    },
+      destroyed()
+    {
+        window.removeEventListener("resize", this.windowResize);
+    },
+    methods:
+    {
+        windowResize: function()
+        {
+            if(document.getElementById('leftColumn') != null && document.getElementById('rightColumn') != null)
+            {
+                var leftColumnPosition = document.getElementById('leftColumn').getBoundingClientRect();
+                var leftX = leftColumnPosition.right;
+                var rightColumnPosition = document.getElementById('rightColumn').getBoundingClientRect();
+                var rightX = rightColumnPosition.left;
+                this.leftoverX = rightX - leftX;
+                //console.log(leftoverX);
+            }
+        },
         openAccordian: function(x)
         {
             x = document.getElementById(x);
@@ -130,14 +152,55 @@ let gameApp = new Vue({
             });
             const playerMarketOrder = await playerMarketOrderRequest.json();
             console.log(playerMarketOrder);
+        },
+
+        updateRegionBar: function()
+        {
+            document.getElementById('regionBar').className = document.getElementById('regionBar').className.replace(" w3-hide", " w3-show");
+            document.getElementById('actButtons').className = document.getElementById('actButtons').className.replace(" w3-show", " w3-hide");
+            document.getElementById('regionBarInfoLeftHeader').innerHTML =  "-=- " + this.currentlySelectedRegion.name;
+            if (this.currentlySelectedRegion.player != null)
+            {
+                document.getElementById('regionBarInfoLeftHeader').innerHTML += " - " + gameApp.game.players.filter(player => player._id == this.currentlySelectedRegion.player)[0].name;
+                if (this.currentlySelectedRegion.player == this.player._id)
+                {
+                    document.getElementById('actButtons').className = document.getElementById('actButtons').className.replace(" w3-hide", " w3-show");
+                }
+            }
+            document.getElementById('regionBarInfoLeftHeader').innerHTML += " -=-";
+            document.getElementById('regionBarInfoLeftBody').innerHTML = "Agriculture: " + this.currentlySelectedRegion.industrialization.agriculture + " | Mining: " + this.currentlySelectedRegion.industrialization.mining + " | Synthetics: " + this.currentlySelectedRegion.industrialization.synthetics;
+            document.getElementById('regionBarInfoLeftBody').innerHTML += "<br>Land: " + this.currentlySelectedRegion.units.land + " | Naval: " + this.currentlySelectedRegion.units.naval + " | Amphibious: " + this.currentlySelectedRegion.units.amphibious;
+            document.getElementById('regionBarInfoLeftBody').innerHTML += "<br>Atom Bombs: " + this.currentlySelectedRegion.units.atomBombs + " | Bioweapons: " + this.currentlySelectedRegion.units.bioweapons + " | Radars: " + this.currentlySelectedRegion.units.radars;
+        },
+
+        attack: async function()
+        {
+            const playerAttackRequest = await fetch(URL + "/player/attack", {
+                method: "POST",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    game: this.game._id,
+                    attackingRegion: this.currentlySelectedRegion,
+                    defendingRegion: "",
+                    player: this.player._id,
+                    units:
+                    {
+                        land: this.currentlySelectedRegion.units.land,
+                        naval: this.currentlySelectedRegion.units.naval,
+                        amphibious: this.currentlySelectedRegion.units.amphibious,
+                        atomBombs: this.currentlySelectedRegion.units.atomBombs,
+                        bioweapons: this.currentlySelectedRegion.units.bioweapons,
+                        radars: this.currentlySelectedRegion.units.radars
+                    }
+                })
+            });
+            const playerAttack = await playerAttackRequest.json();
+            console.log(playerAttack);
         }
     }
-
-    //mounted()
-    //{
-    //    this.canvas = document.getElementById("canvas");
-    //    this.ctx = canvas.getContext("2d");
-    //}
 });
 
 // update the page onload
