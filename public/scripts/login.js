cf180838147af8aae2b782f4c1b52d61cff07474
baiken314 <60582899+baiken314@users.noreply.github.com>
2021-04-30 10:23:36 -0400

    fixed URL

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,4 +1,4 @@
 //const URL = "http://23.130.192.72:8000";
-const URL = "http://localhost:8000";
+const URL = "http://" + window.location.host;
 
 console.log("Starting.....");
--- public/scripts/userpage.js
+++ public/scripts/userpage.js
@@ -1,5 +1,5 @@
 //const URL = "http://23.130.192.72:8000";
-const URL = "http://localhost:8000";
+const URL = "http://" + window.location.host;
 
 console.log("Starting.....");
 
